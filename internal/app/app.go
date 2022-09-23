047ac36aa5fb3ee5a342ea34963761f65e3874ce
voronov <voronov54@gmail.com>
2022-09-23 16:24:32 +0700

    updated logging

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/ArtemVoronov/indefinite-studies-profiles-service/internal/api/grpc/v1/profiles"
 	"github.com/ArtemVoronov/indefinite-studies-profiles-service/internal/api/rest/v1/ping"
@@ -10,18 +12,30 @@ import (
 	"github.com/ArtemVoronov/indefinite-studies-profiles-service/internal/services"
 	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/app"
 	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/services/auth"
+	"github.com/ArtemVoronov/indefinite-studies-utils/pkg/utils"
 	"github.com/gin-contrib/expvar"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
 
 func Start() {
 	app.LoadEnv()
+	logger := app.NewLogrusLogger()
+	logpath := utils.EnvVarDefault("APP_LOGS_PATH", "stdout")
+	if logpath != "stdout" {
+		file, err := os.OpenFile(logpath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("unable init logging: %v", err)
+		}
+		logger.SetOutput(file)
+		defer file.Close()
+	}
 	creds := app.TLSCredentials()
 	go func() {
-		app.StartGRPC(setup, shutdown, app.HostGRPC(), createGrpcApi, &creds)
+		app.StartGRPC(setup, shutdown, app.HostGRPC(), createGrpcApi, &creds, logger)
 	}()
-	app.StartHTTP(setup, shutdown, app.HostHTTP(), createRestApi())
+	app.StartHTTP(setup, shutdown, app.HostHTTP(), createRestApi(logger))
 }
 
 func setup() {
@@ -32,11 +46,11 @@ func shutdown() {
 	services.Instance().Shutdown()
 }
 
-func createRestApi() *gin.Engine {
+func createRestApi(logger *logrus.Logger) *gin.Engine {
 	router := gin.Default()
 	gin.SetMode(app.Mode())
 	router.Use(app.Cors())
-	router.Use(gin.Logger())
+	router.Use(app.JSONLogMiddleware(logger))
 	router.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
 		if err, ok := recovered.(string); ok {
 			c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
